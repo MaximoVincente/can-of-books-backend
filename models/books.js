0556d450de98731a87c2e50e8a8ac4ccc2be0947
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-10-04 17:47:05 -0400

    added C and D in CRUD

--- models/books.js
+++ models/books.js
@@ -6,7 +6,7 @@ const { Schema } = mongoose;
 const bookSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
-    status: {type: Boolean, required: true},
+    status: {type: String, required: true},
 });
 
 const BookModel = mongoose.model('books', bookSchema);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('dotenv').config();
+const { CategoryScale } = require('chart.js');
 const cors = require('cors');
 const express = require('express');
 //bring in mongoose
@@ -8,6 +9,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
+
+//middleware
+app.use(express.json());
+
 
 //Mongoose
 mongoose.connect(process.env.DB_URL); // DB url goes here
@@ -39,7 +44,39 @@ async function getBooks(req, res) {
   }
 }
 
+//Post endpoints
+app.post('/books', postBooks);
+
+async function postBooks(req, res, next) {
+  console.log(req.body);
+  try{
+    const newBook = await Book.create(req.body);
+    res.status(200).send(newBook);
+  }catch(error){
+    next (error);
+  }
+}
+
+//delete book
+app.delete('/books/:id', deleteBook);
+
+async function deleteBook(req, res, next) {
+  const id = req.params.id;
+  console.log(id);
+  try{
+    await Book.findByIdAndDelete(id);
+    res.status(204).send('Delete was Succesful')
+  }catch(error){
+    next(error);
+  }
+}
+
 // catch-all
 app.get('*', (req, res) => {
   res.status(404).send('Not Found');
+});
+
+// Error handling app.use()
+app.use((error, req, res) => {
+  res.status(500).send(error.message);
 });
